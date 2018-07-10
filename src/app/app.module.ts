2e9ba28def4edcb70e21fc121166d9144d1b132f
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-07-10 16:34:15 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WriterComponent } from './writer.component';
+import { WriterService } from './writer.service';
+
+//For InMemory testing
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TestData } from './test-data';
+// import { HttpclntComponent } from './httpclnt/httpclnt.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	  WriterComponent
+	  // HttpclntComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	  HttpClientModule,
+	  ReactiveFormsModule,
+    InMemoryWebApiModule.forRoot(TestData)
   ],
-  providers: [],
+  providers: [WriterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/writer.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+import { Writer } from './writer';
+
+@Injectable()
+export class WriterService {
+	
+	constructor(private http: HttpClient) { }
+	
+	textUrl = "/api/message";
+    getTextMsg(): Observable<string> {
+        return this.http.get(this.textUrl, {responseType: 'text'});
+    }		
+	
+    bookUrl = "/api/books";	
+    getBooks(): Observable<Book[]> {
+        return this.http.get<Book[]>(this.bookUrl);
+    }
+    filterBooks(category: string, year: string): Observable<Book[]> {
+		let httpHeaders = new HttpHeaders()
+		                  .set('Accept', 'application/json');
+        return this.http.get<Book[]>(this.bookUrl + '?category=' + category + '&year=' + year, {
+			        headers: httpHeaders,
+			        responseType: 'json'
+				});
+    }
+	
+	writerUrl = "/api/writer";
+    getWriterWithFavBooks(): Observable<any> {
+        return this.http.get(this.writerUrl, {responseType: 'json'});
+    }	
+    getFavoriteWriter(): Observable<Writer> {
+        return this.http.get<Writer>(this.writerUrl, {responseType: 'json'});
+    }		
+    getFullResponseForWriter(): Observable<HttpResponse<any>> {
+        return this.http.get(this.writerUrl, {
+					   observe: 'response'
+				   });
+    }	
+	
+    myUrl = "/api/invalid";
+    getDataForUrl(): Observable<any> {
+		return this.http.get(this.myUrl);
+    }	
+}
